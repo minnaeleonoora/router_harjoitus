e847db079d531cc649313f8891c872c779860dac
Kilpijärvi Minna <s1900557@edu.bc.fi>
2019-10-11 10:25:43 +0300

    router harjoitus gittiin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Welcome to home!</h1>
     </div>
   );
 }
+const About = () => {
+  return (
+    <div >
+      <h1>How ABOUT you?</h1>
+    </div>
+  );
+}
+const Projects = ({ match }) => {
+  return (
+    <div >
+      <h1>Welcome to my PROJECTS</h1>
+      <ul>
+        <li>
+          <Link to={`${match.url}/project1`}> Project 1</Link>         
+        </li>
+        <li>
+          <Link to={`${match.url}/project2`}> Project 2</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/project3`}> Project 3</Link>
+        </li>
+      </ul>
+      <Route 
+      path={`${match.path}/:name`}
+      render={({ match }) => (
+        <div>
+          {''}
+          <h3> {match.params.name} </h3>
+          </div>
+      )}
+      />
+
+      <Route path={`${match.path}/:name`} 
+      render={ (props) => <Project {...props} />}
+      />
+    </div>
+  );
+}
+
+const Project = ({match}) => {
+  return (
+    <div>
+      <h3>I'm project page {match.params.name}</h3>
+    </div>
+  )
+}
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+
+}
+const App = () => {
+  return (
+    <div >
+      <Router>
+        <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+
 
 export default App;
---
+++ src/simplerouting.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div >
+      <h1>This is HOME page</h1>
+    </div>
+  );
+}
+const About = () => {
+  return (
+    <div >
+      <h1>This is ABOUT page</h1>
+    </div>
+  );
+}
+const Contact = () => {
+  return (
+    <div >
+      <h1>This is CONTACT page</h1>
+    </div>
+  );
+}
+const App = () => {
+  return (
+    <div >
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+        <switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </switch>
+      </Router>
+    </div>
+  );
+}
+
+
+
+export default App;
